e1df9a8cb53800bdfd263c65596fdd126655a7a2
Alejandro Martínez <zen@itram.es>
2023-11-25 13:20:30 +0100

    allow unfree packages in nixpkgs input by default\n\nit is awkward to pass this parameter otherwise when using this as a flake input

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
           else
             with pkgs; [ openblas ]
         );
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs { inherit system; config.allowUnfree = true;  };
         nativeBuildInputs = with pkgs; [ cmake ninja pkg-config ];
         cudatoolkit_joined = with pkgs; symlinkJoin {
           # HACK(Green-Sky): nix currently has issues with cmake findcudatoolkit
